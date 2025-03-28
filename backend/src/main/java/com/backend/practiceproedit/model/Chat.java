fe76c8d7f550c06db1dc755b184cbe4f11e2015d
Nur Izzati <nurizzatisaidi2@gmail.com>
2025-03-29 01:01:41 +0800

    Create a Chat collection
    It creates a chat collection when the Admin had accepted a request and assigns the Project to an editor

---
+++ backend/src/main/java/com/backend/practiceproedit/model/Chat.java
@@ -0,0 +1,38 @@
+package com.backend.practiceproedit.model;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class Chat {
+    private String chatId;
+    private List<String> participantIds;
+    private String projectId;
+
+    public Chat() {
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public List<String> getParticipantIds() {
+        return participantIds;
+    }
+
+    public void setParticipantIds(List<String> participantIds) {
+        this.participantIds = participantIds;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+}

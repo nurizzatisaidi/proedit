bfae914e213876e9b3c772c6ae8e5f31f2edfa32
Nur Izzati <nurizzatisaidi2@gmail.com>
2025-06-22 16:08:24 +0800

    Fixed PayPal Script Loader for Live

--- frontend/src/components/PaypalScriptLoader.js
+++ frontend/src/components/PaypalScriptLoader.js
@@ -1,18 +1,32 @@
 import { useEffect } from "react";
 
-function PaypalScriptLoader() {
+function PaypalScriptLoader({ onScriptLoad }) {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = process.env.REACT_APP_PAYPAL_SCRIPT_URL;
+
+    script.src =
+      // process.env.REACT_APP_PAYPAL_SCRIPT_URL ||
+      "https://www.paypal.com/sdk/js?client-id=AeiFRu0oEAGscyXLn3QLXDFNB1IOC0LJWMhayXv7GrCH1jtsgarVk7Ymop78fBWsj0ZY20UiE4OXpson&currency=MYR";
+
     script.async = true;
+
+    script.onload = () => {
+      console.log("✅ PayPal SDK loaded");
+      if (onScriptLoad) onScriptLoad();
+    };
+
+    script.onerror = () => {
+      console.error("❌ Failed to load PayPal SDK");
+    };
+
     document.body.appendChild(script);
 
     return () => {
-      document.body.removeChild(script); // Clean up on unmount
+      document.body.removeChild(script);
     };
-  }, []);
+  }, [onScriptLoad]);
 
-  return null; // or return your PayPal buttons here
+  return null;
 }
 
 export default PaypalScriptLoader;
